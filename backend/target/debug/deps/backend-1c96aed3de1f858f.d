c52ae13d735beeeba9e5e9f7423f537db146ed77
Yamakou0621 <yamamotoko021@gmail.com>
2025-03-25 17:38:43 +0900

    fisrt commit

---
+++ backend/target/debug/deps/backend-1c96aed3de1f858f.d
@@ -0,0 +1,5 @@
+/Users/kohei/tasky-rs/backend/target/debug/deps/libbackend-1c96aed3de1f858f.rmeta: src/main.rs
+
+/Users/kohei/tasky-rs/backend/target/debug/deps/backend-1c96aed3de1f858f.d: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/backend-8c5ecc3dc12caa12.d
@@ -0,0 +1,5 @@
+/Users/kohei/tasky-rs/backend/target/debug/deps/libbackend-8c5ecc3dc12caa12.rmeta: src/main.rs
+
+/Users/kohei/tasky-rs/backend/target/debug/deps/backend-8c5ecc3dc12caa12.d: src/main.rs
+
+src/main.rs:
